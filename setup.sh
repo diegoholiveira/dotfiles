62416e8210a38ad9d704c9e910ae9b7d3543f29c
Diego Oliveira <contato@diegoholiveira.com>
2014-06-17 18:36:11 -0300

    Saving my sublime preferences under git

--- setup.sh
+++ setup.sh
@@ -9,14 +9,22 @@
 DOTFILES_DIR=~/dotfiles
 DOTFILES="bashrc bash_profile gitconfig gitignore"
 
+echo "Installing dotfiles"
 for file in $DOTFILES; do
     if [ -f ~/".$file" ]; then
-        echo "Removing the old symlink to .$file in home directory."
+        echo "    Removing the old symlink to .$file in home directory."
         rm -rf ~/".$file"
     fi
 
-    echo "Creating symlink to .$file in home directory."
+    echo "    Creating symlink to .$file in home directory."
     ln -s $DOTFILES_DIR/$file ~/".$file"
 done
 
+echo "Setting up Sublime Text 3 preferences and packages"
+rm -rf ~/Library/Application\ Support/Sublime\ Text\ 3/Packages
+rm -rf ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages
+ln -s $DOTFILES_DIR/st3/Packages ~/Library/Application\ Support/Sublime\ Text\ 3/Packages
+ln -s $DOTFILES_DIR/st3/Installed\ Packages ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages
+
+echo "Loading Bash preferences"
 source ~/.bashrc
