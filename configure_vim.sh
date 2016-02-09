72d581951e90350c207a94a180e60efced2fcb0e
Diego Oliveira <contato@diegoholiveira.com>
2016-02-08 23:46:50 -0200

    Add support for Rust lang

--- configure_vim.sh
+++ configure_vim.sh
@@ -25,3 +25,6 @@ git clone git://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutte
 # Install CtrlP
 git clone git@github.com:kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
 
+# Install support for RustLang
+git clone --depth=1 https://github.com/rust-lang/rust.vim.git ~/.vim/bundle/rust.vim
+
