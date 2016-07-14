dd922e18a9c7318fd1efa66e0a0e0ae194ddf880
Diego Oliveira <contato@diegoholiveira.com>
2016-07-13 22:17:57 -0300

    Install Bash 4

--- setup.sh
+++ setup.sh
@@ -7,53 +7,56 @@
 # ----------------------------------------------------------------------------
 
 if [ ! -f /usr/local/bin/brew ]; then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 # ----------------------------------------------------------------------------
-#
+# Install Bash 4
+# ----------------------------------------------------------------------------
+brew install bash
+
+# Add the new shell to the list of allowed shells
+grep -q "/usr/local/bin/bash" /etc/shells
+if [ $? -ne 0 ]; then
+  sudo bash -c 'echo /usr/local/bin/bash >> /etc/shells'
+fi
+
+# Change to the new shell
+chsh -s /usr/local/bin/bash
+
+
+# ----------------------------------------------------------------------------
 # Install git and gitflow
-#
 # ----------------------------------------------------------------------------
 brew install git
 
 
 # ----------------------------------------------------------------------------
-#
 # Install Python
-#
 # ----------------------------------------------------------------------------
 brew install python
 
 
 # ----------------------------------------------------------------------------
-#
 # Update pip and setuptools
-#
 # ----------------------------------------------------------------------------
 pip install --upgrade pip setuptools
 
 
 # ----------------------------------------------------------------------------
-#
 # Install MacVim
-#
 # ----------------------------------------------------------------------------
 brew install macvim --env-std --override-system-vim -—python
 
 
 # ----------------------------------------------------------------------------
-#
 # Install vcprompt
-#
 # ----------------------------------------------------------------------------
 brew install vcprompt
 
 
 # ----------------------------------------------------------------------------
-#
 # Clone the repository to the machine if it's not there yet
-#
 # ----------------------------------------------------------------------------
 if [ ! -d ~/dotfiles ]; then
    cd ~/
