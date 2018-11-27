b76433837f6ff26001c29a5b6da1cd377c0c59d3
Diego Oliveira <contato@diegoholiveira.com>
2018-11-27 12:44:24 -0200

    use ripgrep as search tool

--- fzf/.fzf.bash
+++ fzf/.fzf.bash
@@ -14,4 +14,9 @@ source "/usr/local/opt/fzf/shell/key-bindings.bash"
 
 # Custom search
 # -------------
-export FZF_DEFAULT_COMMAND='fd --type f --hidden --exclude .git'
+export FZF_DEFAULT_COMMAND='rg --files --hidden --follow --glob "!.git/*"'
+export FZF_CTRL_T_COMMAND="${FZF_DEFAULT_COMMAND}"
+
+_fzf_compgen_path() {
+  rg --files --hidden --follow --glob "!.git/*"
+}
